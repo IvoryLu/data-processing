c098b547c7cdd02931c1ac0ff4aaa08dd332f3f4
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-06-18 15:30:59 +0800

    Update add_column.py

--- add_column.py
+++ add_column.py
@@ -1,20 +1,20 @@
-            with open('E:/test_wed/test_result.csv','r') as csvinput:             
-                    reader = csv.reader(csvinput)
-                    total = []
-                    
+with open('E:/test_wed/test_result.csv','r') as csvinput:             
+        reader = csv.reader(csvinput)
+        total = []
+
 #                    row.append('Animal_Detection')
 #                    row.append(score)
 #                    all.append(row)
-                    
-                    for row in reader:
-                        
-                        if row[1] == inputFileNames[iImage]:
-                            row.append('animal')
-                            row.append(score)
-                        total.append(row)
-            csvinput.close()
-            
-            with open('E:/test_wed/test_result.csv','w') as csvoutput:
-                writer = csv.writer(csvoutput, lineterminator='\n')            
-                writer.writerows(total)    
-            csvoutput.close()
+
+        for row in reader:
+
+            if row[1] == inputFileNames[iImage]:
+                row.append('animal')
+                row.append(score)
+            total.append(row)
+csvinput.close()
+
+with open('E:/test_wed/test_result.csv','w') as csvoutput:
+    writer = csv.writer(csvoutput, lineterminator='\n')            
+    writer.writerows(total)    
+csvoutput.close()
