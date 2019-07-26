2f438f5f4bfe8e3008916f0093cc642d00b179a8
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-07-26 16:47:03 +0800

    Update KM.py
    Add Comments

--- KM.py
+++ KM.py
@@ -1,15 +1,21 @@
+#Read in data
 dataset = pd.read_csv('H:/Juan Lu/AF/Outcome/af_bleed.csv')
 
+#Processing Column
 dataset = dataset.drop(["Column","You","Don't","Need"
                          ], axis=1)
-
+#Group Data
 groups = dataset['desired_group_column']
-ix = (groups == 1) # can be anything here number/string
+ix = (groups == 1)  # can be anything here number/string
+                    # this value will be used to classify the data into two groups. 
 
+#Time
 time = dataset['your_time_col']
 
+#Event
 event = dataset['your_event_col']
 
+#Fit the model
 kmf.fit(time[~ix], event[~ix], label = 'Event without Group')
 ax = kmf.plot() 
 #ax = kmf.plot(ax=ax)
