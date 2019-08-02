c1e958c3022fb6c8ccb5f5d176196536107a5f10
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-08-02 16:26:24 +0800

    Add Comment

--- competing_risk_CIF.R
+++ competing_risk_CIF.R
@@ -1,14 +1,20 @@
+# Read Data
 bleed = read.csv("H:/Juan Lu/AF/Outcome/bleed_new.csv")
+# Time
 ftime <- bleed$day_new
+# Status Definition
 fstatus <- factor(bleed$status,levels = c(0,1,2),labels = c("Censored","Bleed","Mortality"))
+#Covariates
 cov <- factor(bleed$AnticoagcomboA, levels = c(0,1),labels = c("No Anticoag","Anticoag") )
-
-
+#Print summary
 print(z <- cuminc(ftime,fstatus,cov,cencode="Censored"))
 summary(z)
+#Plot
 plot(z,col = 1:8, lwd = 3, lty = 1, xlab = "time")
+#No Need for legend Here
 # legend("topleft",c("No anticoag Bleed","anticoag Bleed","No anticoag Death","anticoag Death"),fill=c("black","red","green","blue"))
 
+#Same thing but different Outome Here.
 stroke = read.csv("H:/Juan Lu/AF/Outcome/af_stroke.csv")
 ftime <- stroke$day_new
 fstatus <- factor(stroke$status,levels = c(0,1,2),labels = c("Censored","Stroke","Mortality"))
