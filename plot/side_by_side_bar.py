0f053d1e56424e2dbf9fef76a28dbb5337bd5d38
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-05 20:32:28 +0800

    Update side_by_side_bar.py
    Add more completed version.

--- plot/side_by_side_bar.py
+++ plot/side_by_side_bar.py
@@ -22,3 +22,34 @@ store2_x = [t*element + w*n for element
 plt.bar(store2_x,sales2)
 
 plt.show()
+
+#%%
+from matplotlib import pyplot as plt
+
+unit_topics = ['Limits', 'Derivatives', 'Integrals', 'Diff Eq', 'Applications']
+middle_school_a = [80, 85, 84, 83, 86]
+middle_school_b = [73, 78, 77, 82, 86]
+
+def create_x(t, w, n, d):
+    return [t*x + w*n for x in range(d)]
+#t = 2 # There are two sets of data: A and B
+#w = 0.8 # We generally want bars to be 0.8
+#n = 1 # A is first set of data
+#d = 5 # There are 5 topics we're plotting
+
+school_a_x = create_x(2,0.8,1,5)
+school_b_x = create_x(2,0.8,2,5)
+plt.figure(figsize=(10,8))
+ax = plt.subplot()
+plt.bar(school_a_x, middle_school_a)
+plt.bar(school_b_x, middle_school_b)
+middle_x = [ (a + b) / 2.0 for a, b in zip(school_a_x, school_b_x)]
+ax.set_xticks(middle_x)
+ax.set_xticklabels(unit_topics)
+legends = ["Middle School A", "Middle School B"]
+plt.legend(legends)
+plt.title("Test Averages on Different Units")
+plt.xlabel("Unit")
+plt.ylabel("Test Average")
+plt.savefig("my_side_by_side.png")
+plt.show()
