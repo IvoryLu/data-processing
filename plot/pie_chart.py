445bce0f9a39a8f611f8861e79f8fd5efc93967a
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-07 16:09:52 +0800

    Update pie_chart.py

--- plot/pie_chart.py
+++ plot/pie_chart.py
@@ -19,3 +19,14 @@ plt.axis('equal')
 plt.title("Hardest Topics")
 plt.savefig("my_pie_chart.png")
 plt.show()
+
+#%%
+countries = [447, 66, 64, 49, 45, 28, 25, 20, 19, 17, 17, 279]
+colors = ['lightskyblue', 'gold', 'lightcoral', 'gainsboro', 'royalblue', 'lightpink', 'darkseagreen', 'sienna', 'khaki', 'gold', 'violet', 'yellowgreen']
+explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+plt.pie(countries,explode = explode,colors=colors,
+       shadow=True, startangle=345, autopct='%1.0f%%',
+       pctdistance=1.15)
+plt.title("League of Legends Viewers' Whereabouts")
+plt.legend(labels, loc="right")
+plt.show()
