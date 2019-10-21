bf80232798e45ed9e33b77b334a6fdbf1abdaaa2
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-21 14:16:49 +0800

    Update p-value.py

--- math/p-value.py
+++ math/p-value.py
@@ -1,11 +1,32 @@
 from scipy.stats import ttest_1samp
 import numpy as np
 
+#One Sample T-test
 for i in range(1000): # 1000 experiments
-   #Test One Sample T-test
+   
    #Expected average is 30. 
    tstatistic, pval = ttest_1samp(daily_visitors[i], 30)
    #print the pvalue here:
    print pval
 
 # P-value give us an idea of how confident we can be in a result. 
+
+#Two Sample T-test
+from scipy.stats import ttest_ind
+import numpy as np
+
+week1 = np.genfromtxt("week1.csv",  delimiter=",")
+week2 = np.genfromtxt("week2.csv",  delimiter=",")
+
+week1_mean = np.mean(week1)
+week2_mean = np.mean(week2)
+print(week1_mean)
+print(week2_mean)
+
+week1_std = np.std(week1)
+week2_std = np.std(week2)
+print(week2_std)
+print(week1_std)
+
+tstatistic, pval = ttest_ind(week1, week2)
+print(pval)
