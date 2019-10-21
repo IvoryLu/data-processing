4566e875d3f7d07cda276c67649b6a0620e2b387
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-21 10:53:40 +0800

    Create p-value.py

---
+++ math/p-value.py
@@ -0,0 +1,10 @@
+from scipy.stats import ttest_1samp
+import numpy as np
+
+for i in range(1000): # 1000 experiments
+   #Test One Sample T-test
+   tstatistic, pval = ttest_1samp(daily_visitors[i], 30)
+   #print the pvalue here:
+   print pval
+
+# P-value give us an idea of how confident we can be in a result. 
