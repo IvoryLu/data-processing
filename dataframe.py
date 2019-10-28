f1c98aa4dfe56b3460e4367a81a613a1c7d6b658
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-28 23:16:27 +0800

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -5,3 +5,6 @@ print(dataset.isnull().sum())
 
 #Calculate column frequency
 dataset['EndStageRenal'].value_counts()
+
+#split the column by number
+split_grade = students['grade'].str.split('(\d+)', expand=True)
