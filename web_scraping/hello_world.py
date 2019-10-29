f347782ea7e82918f24290a285a70dbad60fffb0
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-29 23:09:06 +0800

    Update hello_world.py

--- web_scraping/hello_world.py
+++ web_scraping/hello_world.py
@@ -1,10 +1,19 @@
+#Import
 import requests
 from bs4 import BeautifulSoup
 
+#get request
 webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html')
 
+#convert to soup object
 webpage = webpage_response.content
 soup = BeautifulSoup(webpage, "html.parser")
 
+#print the p tag
 print(soup.p)
+#print the content in the p tag
 print(soup.p.string)
+
+#print all the children under the div
+for child in soup.div.children:
+  print(child)
