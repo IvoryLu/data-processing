f63a22f0f2d1aa5b1b75f430d155c3caeec8a46c
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-10-29 14:38:11 +0800

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -8,3 +8,15 @@ dataset['EndStageRenal'].value_counts()
 
 #split the column by number
 split_grade = students['grade'].str.split('(\d+)', expand=True)
+
+#Import multiple files and concatanate into one dataframe
+import glob
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
