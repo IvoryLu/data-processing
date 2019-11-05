5870e1bd5faf596aea37d75485673c6db4e0d98a
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-11-05 20:39:05 +0800

    Create hourglass.py

---
+++ practice/hourglass.py
@@ -0,0 +1,31 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    print(arr)
+    count = 0
+    num_row = len(arr)
+    num_col = len(arr[0])
+    max_count = 0
+    for i, row in enumerate(arr):
+        for j, ele in enumerate(row):
+            if j + 2 < num_col and i + 2 < num_row:
+                count = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+                print(count)
+                if count > max_count:
+                    max_count = count 
+    return max_count
+
+if __name__ == '__main__':
+    arr = []
+
+    result = hourglassSum(arr)
+
+    print(str(result) + '\n')
+
